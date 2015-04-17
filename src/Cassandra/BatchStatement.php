0b2921213be0c07908f1c18bb49492fe52bb0deb
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2015-04-17 12:52:37 -0700

    fix list in doc block

--- src/Cassandra/BatchStatement.php
+++ src/Cassandra/BatchStatement.php
@@ -21,17 +21,19 @@ namespace Cassandra;
 use Cassandra\Exception\InvalidArgumentException;
 
 /**
- * Batch statements are used to execute a series of simple or prepared statements.
+ * Batch statements are used to execute a series of simple or prepared
+ * statements.
+ *
  * There are 3 types of batch statements:
  *  * `Cassandra::BATCH_LOGGED`   - this is the default batch type. This batch
- *     guarantees that either all or none of its statements will be executed.
- *     This behavior is achieved by writing a batch log on the coordinator, which
- *     slows down the execution somewhat.
- *   * `Cassandra::BATCH_UNLOGGED` - this batch will not be verified when executed,
- *     which makes it faster than a `LOGGED` batch, but means that some of its statements
- *     might fail, while others - succeed.
- *   * `Cassandra::BATCH_COUNTER`  - this batch is used for counter updates, which
- *     are, unlike other writes, not idempotent.
+ *    guarantees that either all or none of its statements will be executed.
+ *    This behavior is achieved by writing a batch log on the coordinator,
+ *    which slows down the execution somewhat.
+ *  * `Cassandra::BATCH_UNLOGGED` - this batch will not be verified when
+ *    executed, which makes it faster than a `LOGGED` batch, but means that
+ *    some of its statements might fail, while others - succeed.
+ *  * `Cassandra::BATCH_COUNTER`  - this batch is used for counter updates,
+ *    which are, unlike other writes, not idempotent.
  *
  * @see Cassandra::BATCH_LOGGED
  * @see Cassandra::BATCH_UNLOGGED
