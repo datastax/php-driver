2a3799e28952a7f2103ed6426c927c32c978e147
mikefero <michael.fero@datastax.com>
2015-05-01 10:57:50 -0400

    build: Added PHP v5.3 to help message

--- ext/vc_build.bat
+++ ext/vc_build.bat
@@ -760,7 +760,7 @@ REM Display the help message and exit with error code
   ECHO     !ARGUMENT_DISABLE_CLEAN_BUILD!                   Disable clean build
   ECHO     !ARGUMENT_DISABLE_THREAD_SAFETY!           Disable thread safety
 REM	ECHO     !ARGUMENT_ENABLE_BUILD_PACKAGES! [version]       Enable package generation
-  ECHO     !ARGUMENT_PHP_VERSION! [version]           PHP version 5.4, 5.5, or 5.6
+  ECHO     !ARGUMENT_PHP_VERSION! [version]           PHP version 5.3, 5.4, 5.5, or 5.6
   IF !SYSTEM_ARCHITECTURE! EQU !ARCHITECTURE_32BIT! (
     ECHO     !ARGUMENT_TARGET_ARCHITECTURE_32BIT!                             Target 32-bit build ^(default^)
     ECHO     !ARGUMENT_TARGET_ARCHITECTURE_64BIT!                             Target 64-bit build
