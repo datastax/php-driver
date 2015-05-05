7ce39c48d0945c2e0425db269c4ed8d811cd729a
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2015-05-05 14:18:11 -0700

    remove deprecated functions

--- ext/php_cassandra.h
+++ ext/php_cassandra.h
@@ -24,11 +24,6 @@
 /* Resources */
 #define PHP_CASSANDRA_CLUSTER_RES_NAME    "Cassandra Cluster"
 #define PHP_CASSANDRA_SESSION_RES_NAME    "Cassandra Session"
-#define PHP_CASSANDRA_FUTURE_RES_NAME     "Cassandra Future"
-#define PHP_CASSANDRA_STATEMENT_RES_NAME  "Cassandra Statement"
-#define PHP_CASSANDRA_RESULT_RES_NAME     "Cassandra Result"
-#define PHP_CASSANDRA_PREPARED_RES_NAME   "Cassandra Prepared Statement"
-#define PHP_CASSANDRA_BATCH_RES_NAME      "Cassandra Batch Statement"
 
 extern zend_module_entry cassandra_module_entry;
 #define phpext_cassandra_ptr &cassandra_module_entry
@@ -55,6 +50,8 @@ extern zend_module_entry cassandra_module_entry;
 #ifdef ZTS
 #    include "TSRM.h"
 #endif
+
+zend_class_entry* exception_class(CassError rc);
 
 void throw_invalid_argument(zval* object,
                             const char* object_name,
@@ -87,54 +84,11 @@ void throw_invalid_argument(zval* object,
 
 #include "php_cassandra_types.h"
 
-zend_class_entry* exception_class(CassError rc);
-
 PHP_MINIT_FUNCTION(cassandra);
 PHP_MSHUTDOWN_FUNCTION(cassandra);
 PHP_RINIT_FUNCTION(cassandra);
 PHP_RSHUTDOWN_FUNCTION(cassandra);
 PHP_MINFO_FUNCTION(cassandra);
-
-/* CassSession */
-PHP_FUNCTION(cassandra_session_new);
-PHP_FUNCTION(cassandra_session_free);
-PHP_FUNCTION(cassandra_session_connect);
-PHP_FUNCTION(cassandra_session_connect_keyspace);
-PHP_FUNCTION(cassandra_session_execute);
-PHP_FUNCTION(cassandra_session_prepare);
-PHP_FUNCTION(cassandra_session_execute_batch);
-PHP_FUNCTION(cassandra_session_close);
-
-/* CassFuture */
-PHP_FUNCTION(cassandra_future_free);
-PHP_FUNCTION(cassandra_future_wait);
-PHP_FUNCTION(cassandra_future_wait_timed);
-PHP_FUNCTION(cassandra_future_get_result);
-PHP_FUNCTION(cassandra_future_get_prepared);
-
-/* CassResult */
-PHP_FUNCTION(cassandra_result_free);
-PHP_FUNCTION(cassandra_result_row_count);
-PHP_FUNCTION(cassandra_result_has_more_pages);
-
-/* CassStatement */
-PHP_FUNCTION(cassandra_statement_new);
-PHP_FUNCTION(cassandra_statement_free);
-PHP_FUNCTION(cassandra_statement_bind);
-PHP_FUNCTION(cassandra_statement_set_consistency);
-PHP_FUNCTION(cassandra_statement_set_paging_size);
-PHP_FUNCTION(cassandra_statement_set_serial_consistency);
-PHP_FUNCTION(cassandra_statement_set_paging_state);
-
-/* CassPrepared */
-PHP_FUNCTION(cassandra_prepared_free);
-PHP_FUNCTION(cassandra_prepared_bind);
-
-/* CassBatch */
-PHP_FUNCTION(cassandra_batch_new);
-PHP_FUNCTION(cassandra_batch_free);
-PHP_FUNCTION(cassandra_batch_set_consistency);
-PHP_FUNCTION(cassandra_batch_add_statement);
 
 ZEND_BEGIN_MODULE_GLOBALS(cassandra)
   CassUuidGen*          uuid_gen;
