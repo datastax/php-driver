89fe3322137bdeab6a3239e8f965906f18467b31
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2015-09-11 11:43:33 -0700

    fix links to native protocol docs

--- ext/doc/Cassandra/Exception/ReadTimeoutException.php
+++ ext/doc/Cassandra/Exception/ReadTimeoutException.php
@@ -21,6 +21,6 @@ namespace Cassandra\Exception;
 /**
  * ReadTimeoutException is raised when a coordinator failed to receive acks
  * from the required number of replica nodes in time during a read.
- * @see https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v1.spec#L709-L726 Description of ReadTimeout error in the native protocol spec
+ * @see https://github.com/apache/cassandra/blob/cassandra-2.1/doc/native_protocol_v1.spec#L709-L726 Description of ReadTimeout error in the native protocol spec
  */
 class ReadTimeoutException extends ExecutionException {}
--- ext/doc/Cassandra/Exception/UnavailableException.php
+++ ext/doc/Cassandra/Exception/UnavailableException.php
@@ -23,6 +23,6 @@ namespace Cassandra\Exception;
  * enough replica nodes available to fulfill the request.
  *
  * NOTE: Request has not even been forwarded to the replica nodes in this case.
- * @see https://github.com/apache/cassandra/blob/trunk/doc/native_protocol_v1.spec#L667-L677 Description of the Unavailable error in the native protocol v1 spec.
+ * @see https://github.com/apache/cassandra/blob/cassandra-2.1/doc/native_protocol_v1.spec#L667-L677 Description of the Unavailable error in the native protocol v1 spec.
  */
 class UnavailableException extends ExecutionException {}
