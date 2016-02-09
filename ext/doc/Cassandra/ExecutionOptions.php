6c4996ec70601b3f7717417574fefbdc5950f8c9
Michael Penick <michael.penick@datastax.com>
2016-02-09 14:44:31 -0700

    Added 'retry_policy' to execution options docs

--- ext/doc/Cassandra/ExecutionOptions.php
+++ ext/doc/Cassandra/ExecutionOptions.php
@@ -31,14 +31,15 @@ final class ExecutionOptions
     /**
      * Creates a new options object for execution.
      *
-     * * array['arguments']          array    An array or positional or named arguments
-     * * array['consistency']        int      One of Cassandra::CONSISTENCY_*
-     * * array['timeout']            int|null A number of seconds or null
-     * * array['page_size']          int      A number of rows to include in result for paging
-     * * array['paging_state_token'] string   A string token use to resume from the state of a previous result set
-     * * array['serial_consistency'] int      Either Cassandra::CONSISTENCY_SERIAL or Cassandra::CONSISTENCY_LOCAL_SERIAL
-     * * array['timestamp']          int|string Either an integer or integer string timestamp that represents the number
-     *                                          of microseconds since the epoch.
+     * * array['arguments']          array                  An array or positional or named arguments
+     * * array['consistency']        int                    One of Cassandra::CONSISTENCY_*
+     * * array['timeout']            int|null               A number of seconds or null
+     * * array['page_size']          int                    A number of rows to include in result for paging
+     * * array['paging_state_token'] string                 A string token use to resume from the state of a previous result set
+     * * array['retry_policy']       Cassandra\RetryPolicy  A retry policy that is used to handle server-side failures for this request
+     * * array['serial_consistency'] int                    Either Cassandra::CONSISTENCY_SERIAL or Cassandra::CONSISTENCY_LOCAL_SERIAL
+     * * array['timestamp']          int|string             Either an integer or integer string timestamp that represents the number
+     *                                                      of microseconds since the epoch.
      *
      * @throws Exception\InvalidArgumentException
      *
