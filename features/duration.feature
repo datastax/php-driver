matuszeg/php-dse-driver

PHP-170 - Duration nanos should support a signed 64-bit range

features/core/duration.feature
@@ -1,0 +1,50 @@
+@cassandra-version-3.10
+Feature: Duration
+
+  PHP Driver supports the `Duration` datatype
+
+  Background:
+    Given a running Cassandra cluster
+
+  Scenario: Use the duration type
+    Given the following schema:
+      """cql
+      CREATE KEYSPACE simplex WITH replication = {
+        'class': 'SimpleStrategy',
+        'replication_factor': 1
+      };
+      USE simplex;
+      CREATE TABLE duration (k text PRIMARY KEY, d duration);
+      """
+    And the following example:
+      """php
+      $cluster   = Dse::cluster()->build();
+      $session   = $cluster->connect("simplex");
+
+      $durations = array(
+          array('two_days', new Dse\Duration(2, 0, 0)),
+          array('twelve_hours', new Dse\Duration(0, 12, 0)),
+          array('three_seconds', new Dse\Duration(0, 0, 3 * (1000 ** 3))),
+          array('two_days_twelve_hours_and_three_seconds', new Dse\Duration(2, 12, 3 * (1000 ** 3)))
+      );
+
+      foreach ($durations as $duration) {
+          $options = array('arguments' => $duration);
+          $session->execute("INSERT INTO duration (k, d) VALUES (?, ?)", $options);
+      }
+
+      $rows = $session->execute("SELECT * FROM duration");
+
+      foreach ($rows as $row) {
+        echo "{$row['k']}: {$row['d']}" . PHP_EOL;
+      }
+      """
+    When it is executed
+    Then its output should contain disregarding order:
+      """
+      twelve_hours: 0mo12d0ns
+      three_seconds: 0mo0d3000000000ns
+      two_days_twelve_hours_and_three_seconds: 2mo12d3000000000ns
+      two_days: 2mo0d0ns
+      """
+

features/core/duration_collections.feature
@@ -1,0 +1,73 @@
+@cassandra-version-3.10
+Feature: Duration
+
+  PHP Driver supports the `Duration` datatype in collections
+
+  Background:
+    Given a running Cassandra cluster
+
+  Scenario: Use the duration type in collections
+    Given the following schema:
+      """cql
+      CREATE KEYSPACE simplex WITH replication = {
+        'class': 'SimpleStrategy',
+        'replication_factor': 1
+      };
+      USE simplex;
+      CREATE TYPE duration_user_type (duration1 duration, duration2 duration);
+      CREATE TABLE durations (k text PRIMARY KEY,
+                              duration_list list<duration>,
+                              duration_tuple tuple<duration, duration>,
+                              duration_udt duration_user_type);
+      """
+    And the following example:
+      """php
+      $cluster = Dse::cluster()->build();
+      $session = $cluster->connect("simplex");
+
+      # Construct collection arguments
+
+      $list = Dse\Type::collection(Dse\Type::duration())->create(
+          new Dse\Duration(1, 2, 3)
+      );
+      $list->add(new Dse\Duration(4, 5, 6));
+
+      $tuple = Dse\Type::tuple(Dse\Type::duration(), Dse\Type::duration())->create(
+          new Dse\Duration(1, 2, 3), new Dse\Duration(4, 5, 6)
+      );
+
+      $udt = Dse\Type::userType('duration1', Dse\Type::duration(), 'duration2', Dse\Type::duration())->create(
+          'duration1', new Dse\Duration(1, 2, 3)
+      );
+      $udt->set('duration2', new Dse\Duration(4, 5, 6));
+
+
+      # Insert collections containing durations into table
+      $options = array('arguments' =>
+          array('key1', $list, $tuple, $udt)
+      );
+      $session->execute("INSERT INTO durations
+          (k, duration_list, duration_tuple, duration_udt) VALUES
+          (?, ?, ?, ?)", $options);
+      # Select collections from table and print as WKT
+      $rows = $session->execute("SELECT * FROM durations WHERE k = 'key1'");
+      $row = $rows->first();
+
+
+      $rows = $session->execute("SELECT * FROM durations WHERE k = 'key1'");
+      $row = $rows->first();
+
+      echo 'The list contains durations: [' . implode(', ', $row['duration_list']->values()) . ']' . PHP_EOL;
+      echo 'The tuple contains durations: (' . implode(', ', $row['duration_tuple']->values()) . ')' . PHP_EOL;
+      $asWkt = array_map(function($k, $v) { return $k . ': ' . $v; },
+          array_keys($row["duration_udt"]->values()), $row["duration_udt"]->values());
+      echo 'The udt contains durations: {' . implode(', ', $asWkt) . '}' . PHP_EOL;
+      """
+    When it is executed
+    Then its output should contain disregarding order:
+      """
+      The list contains durations: [1mo2d3ns, 4mo5d6ns]
+      The tuple contains durations: (1mo2d3ns, 4mo5d6ns)
+      The udt contains durations: {duration1: 1mo2d3ns, duration2: 4mo5d6ns}
+      """
+

datastax/php-driver

features/duration.feature
@@ -1,0 +1,51 @@
+@cassandra-version-3.10
+Feature: Duration
+
+  PHP Driver supports the `Duration` datatype
+
+  Background:
+    Given a running Cassandra cluster
+
+  Scenario: Use the duration type
+    Given the following schema:
+      """cql
+      CREATE KEYSPACE simplex WITH replication = {
+        'class': 'SimpleStrategy',
+        'replication_factor': 1
+      };
+      USE simplex;
+      CREATE TABLE duration (k text PRIMARY KEY, d duration);
+      """
+    And the following example:
+      """
+      <?php
+      $cluster   = Cassandra::cluster()->build();
+      $session   = $cluster->connect("simplex");
+
+      $durations = array(
+          array('two_days', new Cassandra\Duration(2, 0, 0)),
+          array('twelve_hours', new Cassandra\Duration(0, 12, 0)),
+          array('three_seconds', new Cassandra\Duration(0, 0, 3 * (1000 ** 3))),
+          array('two_days_twelve_hours_and_three_seconds', new Cassandra\Duration(2, 12, 3 * (1000 ** 3)))
+      );
+
+      foreach ($durations as $duration) {
+          $options = array('arguments' => $duration);
+          $session->execute("INSERT INTO duration (k, d) VALUES (?, ?)", $options);
+      }
+
+      $rows = $session->execute("SELECT * FROM duration");
+
+      foreach ($rows as $row) {
+        echo "{$row['k']}: {$row['d']}" . PHP_EOL;
+      }
+      """
+    When it is executed
+    Then its output should contain these lines in any order:
+      """
+      twelve_hours: 0mo12d0ns
+      three_seconds: 0mo0d3000000000ns
+      two_days_twelve_hours_and_three_seconds: 2mo12d3000000000ns
+      two_days: 2mo0d0ns
+      """
+

features/duration_collections.feature
@@ -1,0 +1,74 @@
+@cassandra-version-3.10
+Feature: Duration
+
+  PHP Driver supports the `Duration` datatype in collections
+
+  Background:
+    Given a running Cassandra cluster
+
+  Scenario: Use the duration type in collections
+    Given the following schema:
+      """cql
+      CREATE KEYSPACE simplex WITH replication = {
+        'class': 'SimpleStrategy',
+        'replication_factor': 1
+      };
+      USE simplex;
+      CREATE TYPE duration_user_type (duration1 duration, duration2 duration);
+      CREATE TABLE durations (k text PRIMARY KEY,
+                              duration_list list<duration>,
+                              duration_tuple tuple<duration, duration>,
+                              duration_udt duration_user_type);
+      """
+    And the following example:
+      """
+      <?php
+      $cluster = Cassandra::cluster()->build();
+      $session = $cluster->connect("simplex");
+
+      # Construct collection arguments
+
+      $list = Cassandra\Type::collection(Cassandra\Type::duration())->create(
+          new Cassandra\Duration(1, 2, 3)
+      );
+      $list->add(new Cassandra\Duration(4, 5, 6));
+
+      $tuple = Cassandra\Type::tuple(Cassandra\Type::duration(), Cassandra\Type::duration())->create(
+          new Cassandra\Duration(1, 2, 3), new Cassandra\Duration(4, 5, 6)
+      );
+
+      $udt = Cassandra\Type::userType('duration1', Cassandra\Type::duration(), 'duration2', Cassandra\Type::duration())->create(
+          'duration1', new Cassandra\Duration(1, 2, 3)
+      );
+      $udt->set('duration2', new Cassandra\Duration(4, 5, 6));
+
+
+      # Insert collections containing durations into table
+      $options = array('arguments' =>
+          array('key1', $list, $tuple, $udt)
+      );
+      $session->execute("INSERT INTO durations
+          (k, duration_list, duration_tuple, duration_udt) VALUES
+          (?, ?, ?, ?)", $options);
+      # Select collections from table and print as WKT
+      $rows = $session->execute("SELECT * FROM durations WHERE k = 'key1'");
+      $row = $rows->first();
+
+
+      $rows = $session->execute("SELECT * FROM durations WHERE k = 'key1'");
+      $row = $rows->first();
+
+      echo 'The list contains durations: [' . implode(', ', $row['duration_list']->values()) . ']' . PHP_EOL;
+      echo 'The tuple contains durations: (' . implode(', ', $row['duration_tuple']->values()) . ')' . PHP_EOL;
+      $asWkt = array_map(function($k, $v) { return $k . ': ' . $v; },
+          array_keys($row["duration_udt"]->values()), $row["duration_udt"]->values());
+      echo 'The udt contains durations: {' . implode(', ', $asWkt) . '}' . PHP_EOL;
+      """
+    When it is executed
+    Then its output should contain these lines in any order:
+      """
+      The list contains durations: [1mo2d3ns, 4mo5d6ns]
+      The tuple contains durations: (1mo2d3ns, 4mo5d6ns)
+      The udt contains durations: {duration1: 1mo2d3ns, duration2: 4mo5d6ns}
+      """
+