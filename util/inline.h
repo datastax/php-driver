28a4f2017061aed2016ad15bb4f0bff7da45196a
Dusan Malusev <dusan@dusanmalusev.dev>
2023-03-08 00:33:54 +0100

    Fixing build on Release
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- util/inline.h
+++ util/inline.h
@@ -2,10 +2,7 @@
 
 #include <php.h>
 
-#if DEBUG
-#define PHP_DRIVER_ALWAYS_INLINE inline
-#define PHP_DRIVER_NEVER_INLINE
-#elif RELEASE
+
 #if defined(__GNUC__)
 #if __GNUC__ >= 3
 #define PHP_DRIVER_ALWAYS_INLINE inline __attribute__((always_inline))
@@ -14,9 +11,6 @@
 #define PHP_DRIVER_ALWAYS_INLINE inline
 #define PHP_DRIVER_NEVER_INLINE
 #endif
-#elif defined(_MSC_VER)
-#define PHP_DRIVER_ALWAYS_INLINE __forceinline
-#define PHP_DRIVER_NEVER_INLINE __declspec(noinline)
 #else
 #if __has_attribute(always_inline)
 #define PHP_DRIVER_ALWAYS_INLINE inline __attribute__((always_inline))
@@ -28,8 +22,4 @@
 #else
 #define PHP_DRIVER_NEVER_INLINE
 #endif
-#endif
-#else
-#define PHP_DRIVER_ALWAYS_INLINE
-#define PHP_DRIVER_NEVER_INLINE
 #endif
