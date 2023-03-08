c57408d9efecf97b9d793df42d514d28b0cc59fe
Dusan Malusev <dusan@dusanmalusev.dev>
2023-03-08 17:02:07 +0100

    fixing compile php script
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- docs/compile-php-debug.sh
+++ docs/compile-php-debug.sh
@@ -1,15 +1,128 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# This script is used to compile PHP with debug symbols.
-
-PHP_VERSION=$1
-PHP_BASE_VERSION=$(echo "$PHP_VERSION" | cut -d. -f1,2)
-PHP_ZTS=$2
-OUTPUT=$3
-
 CFLAGS="-g -ggdb"
 CXXFLAGS="-g -ggdb"
+IS_UBUNTU=0
+
+print_usage() {
+    echo ""
+    echo "Usage: compile-debug-php.sh [OPTION] [ARG]"
+    echo "-v ARG php version"
+    echo "-o ARG output path, default: $HOME"
+    echo "-z Use ZTS"
+    echo ""
+}
+
+check_ubuntu() {
+    if grep "NAME=\"Ubuntu\"" "/etc/os-release" >>/dev/null; then
+        IS_UBUNTU=1
+    fi
+}
+
+compile_php() {
+    local ZTS="$1"
+
+    local config=(
+        --enable-embed=static
+        --enable-phpdbg
+        --enable-opcache
+        --enable-debug
+        --disable-short-tags
+        --with-openssl
+        --with-zlib
+        --with-curl
+        --with-ffi
+        --enable-pcntl
+        --enable-socket
+        --with-zip
+        --with-pic
+        --with-readline
+        --enable-mbstring
+        --with-sqlite3
+        --enable-fpm
+        --enable-calendar
+        --enable-bcmath
+        --with-bz2
+        --with-gmp
+        --with-gettext
+        --with-mysqli
+        --with-pgsql
+        --with-sodium
+        --with-pear
+    )
+
+    if [[ "$ZTS" == "yes" ]]; then
+        local OUTPUT_PATH="$OUTPUT/php/$PHP_BASE_VERSION-debug-zts"
+        config+=("--enable-zts")
+    else
+        local OUTPUT_PATH="$OUTPUT/php/$PHP_BASE_VERSION-debug-nts"
+    fi
+
+    rm -rf "$OUTPUT_PATH"
+
+    wget -O "php-$PHP_VERSION.tar.gz" "https://github.com/php/php-src/archive/refs/tags/php-$PHP_VERSION.tar.gz"
+    tar -C "/tmp" -xzf "php-$PHP_VERSION.tar.gz"
+
+    rm -f "php-$PHP_VERSION.tar.gz"
+
+    if [ "$IS_UBUNTU" -eq 1 ]; then
+        sudo apt-get install \
+            libssl-dev \
+            libsqlite3-dev \
+            zlib1g-dev \
+            libcurl4-openssl-dev \
+            libreadline-dev \
+            libffi-dev \
+            libonig-dev \
+            libbz2-dev \
+            libsodium-dev \
+            libpq-dev \
+            libzip-dev -y
+
+        sudo update-alternatives --force --remove php-cgi "$OUTPUT_PATH/bin/php-cgi"
+        sudo update-alternatives --force --remove php-config "$OUTPUT_PATH/bin/php-config"
+        sudo update-alternatives --force --remove phpize "$OUTPUT_PATH/bin/phpize"
+        sudo update-alternatives --force --remove php "$OUTPUT_PATH/bin/php"
+        sudo update-alternatives --force --remove phpdbg "$OUTPUT_PATH/bin/phpdbg"
+    fi
+
+    pushd "/tmp/php-src-php-$PHP_VERSION" || exit 1
+    ./buildconf --force
+    ./configure CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" --prefix="$OUTPUT_PATH" "${config[@]}"
+    make "-j$(nproc)"
+    make install
+    popd || exit 1
+
+    rm -rf "/tmp/php-src-php-$PHP_VERSION"
+
+    if [ "$IS_UBUNTU" -eq 1 ]; then
+        sudo update-alternatives --install /bin/php-cgi php-cgi "$OUTPUT_PATH/bin/php-cgi" 1
+        sudo update-alternatives --install /bin/php-config php-config "$OUTPUT_PATH/bin/php-config" 1
+        sudo update-alternatives --install /bin/phpize phpize "$OUTPUT_PATH/bin/phpize" 1
+        sudo update-alternatives --install /bin/php php "$OUTPUT_PATH/bin/php" 1
+        sudo update-alternatives --install /bin/phpdbg phpdbg "$OUTPUT_PATH/bin/phpdbg" 1
+    fi
+}
+
+check_deps() {
+    deps="wget make git"
+
+    for dep in $deps; do
+        [ -z "$(command -v "$dep")" ] && echo "Unsatisfied dependency: $dep" && exit 1
+    done
+}
+
+check_deps && check_ubuntu
+
+while getopts "v:zo:" option; do
+    case "$option" in
+    "v") PHP_VERSION="$OPTARG" ;;
+    "z") PHP_ZTS="yes" ;;
+    "o") OUTPUT="$OPTARG" ;;
+    *) print_usage ;;
+    esac
+done
 
 if [[ -z "$PHP_ZTS" ]]; then
     PHP_ZTS="no"
@@ -19,153 +132,15 @@ if [[ -z "$OUTPUT" ]]; then
     OUTPUT="$HOME"
 fi
 
-printf "OUTPUT: %s\n" "$OUTPUT"
-printf "ZTS: %s\n" "$PHP_ZTS"
-
 if [[ -z "$PHP_VERSION" ]]; then
-    echo "Usage: compile-debug-php.sh PHP_VERSION [ZTS=yes|no] [OUTPUT=$HOME]"
+    print_usage
     exit 1
 fi
 
-grep "NAME=\"Ubuntu\"" "/etc/os-release" >>/dev/null
-if [[ $? == 0 ]]; then
-    sudo apt-get install \
-        libssl-dev \
-        libsqlite3-dev \
-        zlib1g-dev \
-        libcurl4-openssl-dev \
-        libreadline-dev \
-        libffi-dev \
-        libonig-dev \
-        libbz2-dev \
-        libsodium-dev \
-        libpq-dev \
-        libzip-dev -y
+PHP_BASE_VERSION=$(echo "$PHP_VERSION" | cut -d. -f1,2)
+
+compile_php "no"
+
+if [[ $PHP_ZTS == "yes" ]]; then
+    compile_php "yes"
 fi
-
-PHP_SOURCE_CODE="https://github.com/php/php-src/archive/refs/tags/php-$PHP_VERSION.tar.gz"
-
-# Download PHP source code
-wget -O "php-$PHP_VERSION.tar.gz" "$PHP_SOURCE_CODE"
-tar -C "/tmp" -xzf "php-$PHP_VERSION.tar.gz"
-
-rm -f "php-$PHP_VERSION.tar.gz"
-rm -rf "$OUTPUT/php/$PHP_VERSION-$PHP_MODE-nts"
-
-grep "NAME=\"Ubuntu\"" "/etc/os-release" >>/dev/null
-if [[ $? == 0 ]]; then
-    sudo update-alternatives --force --remove php-cgi "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/php-cgi"
-    sudo update-alternatives --force --remove php-config "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/php-config"
-    sudo update-alternatives --force --remove phpize "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/phpize"
-    sudo update-alternatives --force --remove php "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/php"
-    sudo update-alternatives --force --remove phpdbg "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/phpdbg"
-fi
-
-pushd "/tmp/php-src-php-$PHP_VERSION" || exit 1
-
-./buildconf --force
-./configure \
-    CFLAGS="$CFLAGS" \
-    CXXFLAGS="$CXXFLAGS" \
-    --enable-debug \
-    --prefix="$OUTPUT/php/$PHP_VERSION-debug-nts" \
-    --enable-embed=static \
-    --enable-opcache \
-    --enable-debug \
-    --disable-short-tags \
-    --with-openssl \
-    --with-zlib \
-    --with-curl \
-    --with-ffi \
-    --enable-pcntl \
-    --enable-intl \
-    --enable-sockets \
-    --with-zip \
-    --with-pic \
-    --with-readline \
-    --enable-mbstring \
-    --with-sqlite3 \
-    --enable-fpm \
-    --enable-calendar \
-    --enable-bcmath \
-    --with-bz2 \
-    --with-gmp \
-    --with-gettext \
-    --with-mysqli \
-    --with-pgsql \
-    --with-sodium \
-    --with-pear \
-    --with-pcre-regex
-
-make -j"$(nproc)"
-make install
-
-if [[ "$PHP_ZTS" == "yes" ]]; then
-    rm -rf "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts"
-
-    grep "NAME=\"Ubuntu\"" "/etc/os-release" >>/dev/null
-    if [[ $? == 0 ]]; then
-        sudo update-alternatives --force --remove php-cgi "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/php-cgi"
-        sudo update-alternatives --force --remove php-config "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/php-config"
-        sudo update-alternatives --force --remove phpize "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/phpize"
-        sudo update-alternatives --force --remove php "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/php"
-        sudo update-alternatives --force --remove phpdbg "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/phpdbg"
-    fi
-
-    make clean && git clean -X -f -d && ./buildconf --force
-    ./configure \
-        CFLAGS="$CFLAGS" \
-        CXXFLAGS="$CXXFLAGS" \
-        --prefix="$OUTPUT/php/$PHP_BASE_VERSION-debug-zts" \
-        --enable-embed=static \
-        --enable-zts \
-        --enable-phpdbg \
-        --enable-opcache \
-        --enable-debug \
-        --disable-short-tags \
-        --with-openssl \
-        --with-zlib \
-        --with-curl \
-        --with-ffi \
-        --enable-pcntl \
-        --enable-socket \
-        --with-zip \
-        --with-pic \
-        --with-readline \
-        --enable-mbstring \
-        --with-sqlite3 \
-        --enable-fpm \
-        --enable-calendar \
-        --enable-bcmath \
-        --with-bz2 \
-        --with-gmp \
-        --with-gettext \
-        --with-mysqli \
-        --with-pgsql \
-        --with-sodium \
-        --with-pear
-fi
-
-make -j"$(nproc)"
-make install
-popd || exit 1
-
-grep "NAME=\"Ubuntu\"" "/etc/os-release" >>/dev/null
-if [[ $? == 0 ]]; then
-    sudo update-alternatives --install /bin/php-cgi php-cgi "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/php-cgi" 1
-    sudo update-alternatives --install /bin/php-cgi php-cgi "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/php-cgi" 1
-
-    sudo update-alternatives --install /bin/php-config php-config "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/php-config" 1
-    sudo update-alternatives --install /bin/php-config php-config "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/php-config" 1
-
-    sudo update-alternatives --install /bin/phpize phpize "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/phpize" 1
-    sudo update-alternatives --install /bin/phpize phpize "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/phpize" 1
-
-    sudo update-alternatives --install /bin/php php "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/php" 1
-    sudo update-alternatives --install /bin/php php "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/php" 1
-
-    sudo update-alternatives --install /bin/phpdbg phpdbg "$OUTPUT/php/$PHP_BASE_VERSION-debug-nts/bin/phpdbg" 1
-    sudo update-alternatives --install /bin/phpdbg phpdbg "$OUTPUT/php/$PHP_BASE_VERSION-debug-zts/bin/phpdbg" 1
-fi
-
-rm -rf "/tmp/php-src-php-$PHP_VERSION"
